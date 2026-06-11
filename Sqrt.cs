9d5b55cbcbbd1b07406e733a4d99017edf7c33ae
VasOleMil <37337666+VasOleMil@users.noreply.github.com>
2026-06-11 15:44:04 +0300

    formatting and commenting
    visual improve

--- Sqrt.cs
+++ Sqrt.cs
@@ -4,29 +4,29 @@ public class Program
 {
   public static void Main()
   {
-    Console.WriteLine("{0}", Sqrt(2));
+    Console.WriteLine("{0}", Sqrt(2D));
   }
 
   static double Sqrt(double S)
   {
-    double s, x, c;
+    double c, x, s;
 
     if(S <= 0D)
     {
-      return S==0?0D:-1D;
+      return (S == 0D)? +0D: -1D;
     }
     else
     {
-      c=S>1D?1D:-1D;
+      c = (S > 1D)? +1D: -1D;
 
-      x=S;s=x*x-S; s=(2D*x*s)/(4D*S+3D*s);
+      x = S; s = x * x - S; s = (2D * x * s)/(4D * S + 3D * s);
 
-      while(x+c*s>x)
+      while(x + c * s  > x)
       {
-      x-=s;s=x*x-S; s=(2D*x*s)/(4D*S+3D*s);
+      x-= s; s = x * x - S; s = (2D * x  *s)/(4D  *S + 3D * s);
       }
       
-      return x*3D/4D+(x/4D-s);
+      return x * 3D / 4D + (x / 4D - s);
     }
   }
 }
